4fb02f5e152ad3be124f99e0606ff80aee995162
DevArslan <arslan.gareev.2017@gmail.com>
2020-06-12 03:12:29 +0300

    feat: add routing, auth form, main page, structure component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,44 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from "@angular/router";
 
 import { AppComponent } from './app.component';
+import { AuthorizationComponent } from './authorization/authorization.component';
+import { ProfileComponent } from './profile/profile.component';
+import { ControlPanelComponent } from './control-panel/control-panel.component';
+import { StructuresComponent } from './structures/structures.component';
+import { StructuresCardComponent } from './structures-card/structures-card.component';
+import { MembersComponent } from './members/members.component';
+
+
+const appRouter: Routes = [
+  {
+    path: 'auth',
+    component: AuthorizationComponent
+  },
+  {
+    path: 'structures',
+    component: StructuresComponent
+  },
+  {
+    path: 'members',
+    component: MembersComponent
+  },
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AuthorizationComponent,
+    ProfileComponent,
+    ControlPanelComponent,
+    StructuresComponent,
+    StructuresCardComponent,
+    MembersComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRouter),
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/structures-card/structures-card.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-structures-card',
+  templateUrl: './structures-card.component.html',
+  styleUrls: ['./structures-card.component.scss']
+})
+export class StructuresCardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
