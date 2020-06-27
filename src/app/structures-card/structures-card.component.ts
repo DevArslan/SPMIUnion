cd48679c6db51a1cc05ec1da92b94d1fd442373b
DevArslan <arslan.gareev.2017@gmail.com>
2020-06-28 02:11:18 +0300

    update: reactive structures table

--- src/app/structures-card/structures-card.component.ts
+++ src/app/structures-card/structures-card.component.ts
@@ -23,53 +23,7 @@ export class StructuresCardComponent implements OnInit {
   }
   selectedDataStructures: string[] = []
   selectedDataStructuresUsers: number[] = []
-
-
-  data: { faculty: string, name: string, users: string, structures: { structureName: string, structureUsers: number }[] }[] = [
-    {
-      'faculty': 'Геологоразведочный факультет', 'name': 'Ivan', 'users': '212', 'structures': [
-        { 'structureName': 'Кафедра геофизических и геохимических методов поисков и разведки месторождений полезных ископаемых', 'structureUsers': 11 },
-        { 'structureName': 'Кафедра геологии и разведки месторождений полезных ископаемых', 'structureUsers': 21 },
-        { 'structureName': 'Кафедра минералогии, кристаллографии и петрографии', 'structureUsers': 11 },
-        { 'structureName': 'Кафедра исторической и динамической геологии', 'structureUsers': 3 },
-        { 'structureName': 'Кафедра гидрогеологии и инженерной геологии', 'structureUsers': 1 },
-        { 'structureName': 'Кафедра геологии нефти и газа', 'structureUsers': 14 },
-      ]
-    },
-    {
-      'faculty': 'Горный факультет', 'name': 'Петров', 'users': '32', 'structures': [
-        { 'structureName': 'Кафедра разработки месторождений полезных ископаемых', 'structureUsers': 11 },
-        { 'structureName': 'Кафедра безопасности производств', 'structureUsers': 21 },
-        { 'structureName': 'Кафедра взрывного дела', 'structureUsers': 11 },
-        { 'structureName': 'Кафедра геоэкологии', 'structureUsers': 3 },
-      ]
-    },
-    {
-      'faculty': 'Механико-машиностроительный факультет', 'name': 'Сидоров', 'users': '17', 'structures': [
-        { 'structureName': 'Кафедра машиностроения', 'structureUsers': 11 },
-        { 'structureName': 'Кафедра метрологии, приборостроения и управления качеством', 'structureUsers': 21 },
-        { 'structureName': 'Кафедра материаловедения и технологии художественных изделий', 'structureUsers': 11 },
-        { 'structureName': 'Кафедра транспортно-технологических процессов и машин', 'structureUsers': 3 },
-      ]
-    },
-    {
-      'faculty': 'Нефтегазовый факультет', 'name': 'Иванов', 'users': '92', 'structures': [
-        { 'structureName': 'Кафедра разработки и эксплуатации нефтяных и газовых месторождений', 'structureUsers': 11 },
-        { 'structureName': 'Кафедра транспорта и хранения нефти и газа', 'structureUsers': 21 },
-        { 'structureName': 'Кафедра бурения скважин', 'structureUsers': 11 },
-      ]
-    },
-    {
-      'faculty': 'Факультет переработки минерального сырья', 'name': 'Петров', 'users': '132', 'structures': [
-        { 'structureName': 'Кафедра строительства горных предприятий и подземных сооружений', 'structureUsers': 11 },
-        { 'structureName': 'Кафедра маркшейдерского дела', 'structureUsers': 21 },
-        { 'structureName': 'Кафедра инженерной геодезии', 'structureUsers': 11 },
-        { 'structureName': 'Кафедра архитектуры', 'structureUsers': 3 },
-        { 'structureName': 'Кафедра гидрогеологии и инженерной геологии', 'structureUsers': 1 },
-        { 'structureName': 'Кафедра механики', 'structureUsers': 14 },
-      ]
-    }
-  ]
+  data: { faculty: string, name: string, users: string, structures: { structureName: string, structureUsers: number }[] }[]
 
   dropdown: boolean = false;
 
@@ -91,6 +45,7 @@ export class StructuresCardComponent implements OnInit {
   ngOnInit(): void {
     // Подписка на меню навигации
     this.structureRouting.postData$.subscribe((faculty) => {
+      this.data = this.structureRouting.data
       this.data.forEach(element => {
         if (faculty == element.faculty) {
           this.selectedData = element;
--- src/app/structures-table/structures-table.component.ts
+++ src/app/structures-table/structures-table.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { StructuresRoutingService } from "src/app/shared/structures-routing.service";
 
 @Component({
   selector: 'app-structures-table',
@@ -7,16 +8,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class StructuresTableComponent implements OnInit {
 
-  data: {id: string, name: string, structure: string, users: string, dynamic: string}[] = [
-    {'id':'1', 'name':'Кафедра разработки месторождений полезных ископаемых','structure':'Горный факультет','users':'12','dynamic':'+12'},
-    {'id':'2', 'name':'Кафедра безопасности производств','structure':'Горный факультет','users':'53','dynamic':'+12'},
-    {'id':'3', 'name':'Кафедра взрывного дела','structure':'Горный факультет','users':'22','dynamic':'+12'},
-    {'id':'4', 'name':'Кафедра геоэкологии','structure':'Горный факультет','users':'11','dynamic':'+12'}
-  ]
+  // data: {id: string, name: string, structure: string, users: string, dynamic: string}[] = [
+  //   {'id':'1', 'name':'Кафедра разработки месторождений полезных ископаемых','structure':'Горный факультет','users':'12','dynamic':'+12'},
+  //   {'id':'2', 'name':'Кафедра безопасности производств','structure':'Горный факультет','users':'53','dynamic':'+12'},
+  //   {'id':'3', 'name':'Кафедра взрывного дела','structure':'Горный факультет','users':'22','dynamic':'+12'},
+  //   {'id':'4', 'name':'Кафедра геоэкологии','structure':'Горный факультет','users':'11','dynamic':'+12'}
+  // ]
+  data: { faculty: string, name: string, users: string, structures: { structureName: string, structureUsers: number }[] }[]
+  selectedData: { faculty: string, name: string, users: string, structures: { structureName: string, structureUsers: number }[] } = {
+    'faculty': 'Геологоразведочный факультет', 'name': 'Ivan', 'users': '212', 'structures': [
+      { 'structureName': 'Кафедра геофизических и геохимических методов поисков и разведки месторождений полезных ископаемых', 'structureUsers': 11 },
+      { 'structureName': 'Кафедра геологии и разведки месторождений полезных ископаемых', 'structureUsers': 21 },
+      { 'structureName': 'Кафедра минералогии, кристаллографии и петрографии', 'structureUsers': 11 },
+      { 'structureName': 'Кафедра исторической и динамической геологии', 'structureUsers': 3 },
+      { 'structureName': 'Кафедра гидрогеологии и инженерной геологии', 'structureUsers': 1 },
+      { 'structureName': 'Кафедра геологии нефти и газа', 'structureUsers': 14 },
+    ]
+  }
 
-  constructor() { }
+  constructor(private structureRouting: StructuresRoutingService) {}
 
   ngOnInit(): void {
+
+    this.structureRouting.postData$.subscribe((faculty) => {
+      this.data = this.structureRouting.data
+      this.data.forEach(element => {
+        if (faculty == element.faculty) {
+          this.selectedData = element;
+        }
+      })
+    })
+
+
   }
 
 
