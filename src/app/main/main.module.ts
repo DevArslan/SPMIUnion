ff9e481a0ae799cb90d6a3210ff9eba01ff20ebd
DevArslan <arslan.gareev.2017@gmail.com>
2020-08-03 03:27:51 +0300

    delete: удалил лишние файлы для модальных окон

--- src/app/main/main.module.ts
+++ src/app/main/main.module.ts
@@ -2,15 +2,12 @@ import { NgModule } from '@angular/core';
 import { MainComponent } from './main.component';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
-// import { MembersModule } from "./pages/members/members.module";
-
 import { ApiServiceService } from '../shared/api-service.service';
 import { MainRoutingModule } from './main-router.module';
 import { ProfileComponent } from './profile/profile.component';
 import { ControlPanelComponent } from './nav/control-panel/control-panel.component';
 import { StructuresComponent } from './pages/structures/structures.component';
 import { StructuresCardComponent } from './pages/structures/structures-card/structures-card.component';
-// import { MembersComponent } from './pages/members/members.component';
 import { StructuresAddModalComponent } from './pages/structures/modals/structures-add-modal/structures-add-modal.component';
 import { MembersTableComponent } from './pages/members/members-table/members-table.component';
 import { MembersComponent } from "./pages/members/members.component";
@@ -21,15 +18,6 @@ import { StructuresNavFilterPipe } from '../pipes/structures-nav-filter.pipe';
 import { StructuresTableComponent } from './pages/structures/structures-table/structures-table.component';
 import { UsersComponent } from './pages/users/users.component';
 import { UsersTableComponent } from './pages/users/users-table/users-table.component';
-import { SubDepartmentsAddModalComponent } from './pages/structures/subdepartments/modals/sub-departments-add-modal/sub-departments-add-modal.component';
-
-import { UsersAddModalComponent } from './pages/users/modals/users-add-modal/users-add-modal.component';
-import { UsersDeleteModalComponent } from './pages/users/modals/users-delete-modal/users-delete-modal.component';
-import { UsersEditModalComponent } from './pages/users/modals/users-edit-modal/users-edit-modal.component';
-import { StructuresDeleteModalComponent } from './pages/structures/modals/structures-delete-modal/structures-delete-modal.component';
-import { SubDepartmentsDelModalComponent } from './pages/structures/subdepartments/modals/sub-departments-del-modal/sub-departments-del-modal.component';
-import { SubDepartmentsEditModalComponent } from './pages/structures/subdepartments/modals/sub-departments-edit-modal/sub-departments-edit-modal.component';
-// import { ModalComponent } from "./pages/members/modal/modal.component";
 import { MembersModalComponent } from './pages/members/members-modal/members-modal.component';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { DeleteModalComponent } from './delete-modal/delete-modal.component';
@@ -53,13 +41,6 @@ import { SubdepartmentsModalComponent } from './pages/structures/subdepartments/
     StructuresTableComponent,
     UsersComponent,
     UsersTableComponent,
-    SubDepartmentsAddModalComponent,
-    UsersAddModalComponent,
-    UsersDeleteModalComponent,
-    UsersEditModalComponent,
-    StructuresDeleteModalComponent,
-    SubDepartmentsDelModalComponent,
-    SubDepartmentsEditModalComponent,
     DeleteModalComponent,
     UsersModalComponent,
     SubdepartmentsModalComponent,
