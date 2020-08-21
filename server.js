ed0a913c95406c096359f057094d03b008d648ba
DevArslan <arslan.gareev.2017@gmail.com>
2020-08-21 20:32:13 +0300

    change to deploy version

--- server.js
+++ server.js
@@ -1,15 +1,15 @@
 //Install express server
-const express = require("express");
-const path = require("path");
+const express = require('express');
+const path = require('path');
 
 const app = express();
 
 // Serve only the static files form the angularapp directory
-app.use(express.static(__dirname + "/dist"));
+app.use(express.static(__dirname + '/dist/SPMIUnion'));
 
-app.get("/*", function (req, res) {
-  res.sendFile(path.join(__dirname + "/dist/index.html"));
-});
+app.get('/*', function(req,res) {
+
+res.sendFile(path.join(__dirname+'/dist/SPMIUnion/index.html'));
 
 // Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 4848);
+app.listen(process.env.PORT || 8080);
--- src/app/CONFIG.ts
+++ src/app/CONFIG.ts
@@ -1,4 +1,4 @@
-// export const BASE_URL = 'https://digital.spmi.ru/profsouz_test/api/v1/';
-export const BASE_URL = 'https://digital.spmi.ru/profsouz_backend/api/v1/';
+export const BASE_URL = 'https://digital.spmi.ru/profsouz_test/api/v1/';
+// export const BASE_URL = 'https://digital.spmi.ru/profsouz_backend/api/v1/';
 export const STORAGE_KEY = 'userData';
 export const ADMIN_ROLE = 'Administrator';
